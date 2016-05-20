078341036d3ef5ebdccf6567a0be83b998061aed
Shawn Stovall <stovall.mail@gmail.com>
2016-05-19 22:22:04 -0400

    Added block capability

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,22 +1,25 @@
 # Method to complete a basic bubble sort with slight improvement from Wikipedia: Bubble Sort
-def bubble_sort(list)
+# Takes a block for custom compare strategies.
+def bubble_sort(list, &compare)
   last_sorted = list.length - 1
   sorted = list
+  compare ||= Proc.new { |x, y| x <=> y }
 
   until last_sorted == 0
-    sorted, last_sorted = bubble(sorted, last_sorted)
+    sorted, last_sorted = bubble(sorted, last_sorted, &compare)
   end
 
   return sorted
 end
 
 # Method to complete one iteration for a bubble sort
+# Takes a block for custom compare strategies.
 def bubble(list, last_sorted)
   result = list
   recent_sort = 0
   
   for i in 0...last_sorted
-    if result[i] > result[i + 1]
+    if yield(result[i], result[i + 1]) > 0
       swap!(result, i, i + 1)
       recent_sort = i
     end
